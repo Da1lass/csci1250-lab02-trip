5257ad67eead7c88db8e77434d4e5db8d4465b8f
Dallas <da1las.sutton00@gmail.com>
2026-09-23 10:23:56 -0400

    Gas Calculations Done

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Name: Dallas Sutton
+ * Course: CSCI 1250, Section 001
+ * Assignment: Lab 2, Trip Calculator
+ * Date: September 23, 2026
+ * Description: Prints a greeting and a short introduction to the console.
+ */
+
+// input variables for gasCalc
+double roundTripMiles; 
+double milesPerGallon;
+decimal pricePerGallon;
+
+//calculation and output variables for gasCalc
+double gallonsNeeded;
+decimal fuelCost;
+
+//Reading gasCalc inputs
+
+System.Console.Write("Round miles? ");
+roundTripMiles = Convert.ToDouble(System.Console.ReadLine());
+
+System.Console.WriteLine("");
+
+System.Console.Write("Miles per gallon? ");
+milesPerGallon = Convert.ToDouble(System.Console.ReadLine());
+
+System.Console.WriteLine("");
+
+System.Console.Write("Price per gallon? ");
+pricePerGallon = Convert.ToDecimal(System.Console.ReadLine());
+
+System.Console.WriteLine($"{roundTripMiles},{milesPerGallon},{pricePerGallon}"); //delete line once done testing
+
+//Calculating cost
+gallonsNeeded = roundTripMiles / milesPerGallon;
+fuelCost = Convert.ToDecimal(gallonsNeeded) * pricePerGallon;
+
+//Printing gasCalc Outputs
+System.Console.WriteLine($"Gallons needed: {gallonsNeeded.ToString("F2")}");
+System.Console.WriteLine($"Fuel Cost: {fuelCost.ToString("C")}");
