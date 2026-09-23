f90757caba8026af3112786b607595655033be84
Dallas <da1las.sutton00@gmail.com>
2026-09-23 10:46:17 -0400

    Pizza party calculations + general formatting cleanup

--- Program.cs
+++ Program.cs
@@ -30,12 +30,47 @@ System.Console.WriteLine("");
 System.Console.Write("Price per gallon? ");
 pricePerGallon = Convert.ToDecimal(System.Console.ReadLine());
 
-System.Console.WriteLine($"{roundTripMiles},{milesPerGallon},{pricePerGallon}"); //delete line once done testing
-
 //Calculating cost
 gallonsNeeded = roundTripMiles / milesPerGallon;
 fuelCost = Convert.ToDecimal(gallonsNeeded) * pricePerGallon;
 
 //Printing gasCalc Outputs
+System.Console.WriteLine("");
 System.Console.WriteLine($"Gallons needed: {gallonsNeeded.ToString("F2")}");
 System.Console.WriteLine($"Fuel Cost: {fuelCost.ToString("C")}");
+
+// input variables for pizzaParty
+int peopleAttending;
+int pizzasNeeded;
+decimal pricePerPizza;
+
+//calculation and output variables for gasCalc
+int totalSLices;
+double slicePerPerson;
+decimal totalPizzaCost;
+const int SLICES_PER_PIZZA = 8;
+
+//Reading pizzaParty inputs
+System.Console.Write("How many people are attending? ");
+peopleAttending = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("");
+
+System.Console.Write("How many pizzas are being purchased? ");
+pizzasNeeded = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("");
+
+System.Console.Write("Price per pizza? ");
+pricePerPizza = Convert.ToDecimal(System.Console.ReadLine());
+
+//Calculating cost
+totalSLices = pizzasNeeded * SLICES_PER_PIZZA;
+slicePerPerson = totalSLices / peopleAttending;
+totalPizzaCost = pizzasNeeded * pricePerPizza;
+
+//Printing gasCalc Outputs
+System.Console.WriteLine("");
+System.Console.WriteLine($"Total slices: {totalSLices.ToString("")}");
+System.Console.WriteLine($"Slices per person: {fuelCost.ToString("F2")}");
+System.Console.WriteLine($"Pizza Cost: {totalPizzaCost.ToString("C")}");
